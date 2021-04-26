b97889575e8ff3500ff3655c75e129e343e08366
Gurjind <32732383+Gurjind@users.noreply.github.com>
2021-04-26 15:22:48 +0530

    Create footer_landing.php

---
+++ landing_inc/footer_landing.php
@@ -0,0 +1,14 @@
+<?php
+$footer='
+		<footer class="small">
+			<div class="container">
+				<p class="text-center py-3 mb-0">Copyright &copy; 2021</p>
+			</div>
+		</footer>
+		
+		<script src="../landing-assets/js/jquery-3.5.1.min.js"></script>
+		<script src="../landing-assets/js/popper.min.js"></script>
+		<script src="../landing-assets/js/bootstrap.min.js"></script>
+		<script src="../landing-assets/js/custom.js"></script>
+	</body>
+</html>	';
