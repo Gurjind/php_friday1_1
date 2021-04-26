68048ca88e455dfdb3059ff2cdee8a741d843d87
Gurjind <32732383+Gurjind@users.noreply.github.com>
2021-04-26 15:22:27 +0530

    Create header_landing.php

---
+++ landing_inc/header_landing.php
@@ -0,0 +1,63 @@
+<?php
+$header='
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	
+	<meta charset="utf-8"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1"/>
+				
+		<title>'.$title.'</title>
+		<meta name="description" content="'.$desc.'"/> 
+		<link rel="canonical" href="'.$canonical.'"/>
+		
+		
+		
+		
+		<link rel="stylesheet" href="../landing-assets/css/bootstrap.css"/>
+          
+          <link rel="stylesheet" href="../landing-assets/css/all.min.css"/>
+		
+	</head>
+	<body>
+		<header>
+            <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
+                <div class="container">
+                    <a class="navbar-brand font-weight-bold" href="#"><img src="../landing-assets/images/logo.jpg" width="200px" alt/></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ml-auto small font-weight-bold">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">About</a>
+                            </li>
+                            
+							<li class="nav-item">
+                                <a class="nav-link" href="#">Products</a>
+                            </li>
+							<li class="nav-item">
+                                <a class="nav-link" href="#">Blogs</a>
+                            </li>
+							<li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Dropdown
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item small" href="#">Dropdown 1</a>
+                                </div>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contact</a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>	
+		
+		<div cass="clearfix"></div>';
